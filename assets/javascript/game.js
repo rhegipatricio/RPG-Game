9a08fbed075201729789a91b03599c4f6966671d
rhegipatricio <rhegi.patricio@gmail.com>
2017-05-29 22:18:56 -0400

    set possible functions for Mysterious Titan

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,3 +11,21 @@ var eren 	 = 100
 var annie 	 = 120
 var armored  = 150
 var colossus = 180
+
+//When Mysterious Titan is clicked, these functions take place
+	$("#eren").on("click" function (){
+		$("#characterCard").replaceWith("<h2> Your Character </h2>", $("#eren"));
+		$("#eren").removeClass("characterCard").addClass("playerChoice");
+		$("#enemyChoice").replaceWith($("#annie, #armored, #colossus"));
+		$("#annie, #armored, #colossus").removeClass("characterCard").addClass("remainingOpponents");
+		$("#eren").attr("id", "erenPlayer");
+		$("#annie").attr("id", "annieOpponent");
+		$("#armored").attr("id", "armoredOpponent");
+		$("#colossus").attr("id", "colossusOpponent");
+		opponentChoice(this.id);
+	});
+
+//When Female TItan is clicked, these functions take place
+	$("#annie").on("click" function (){
+
+	});
